cddf368d7b5915a7f20ae0cf44b1b143443c1536
AoyamaNaoki <45162737+AoyamaNaoki@users.noreply.github.com>
2018-11-28 11:23:37 +0900

    Practice5-2

---
+++ ProjectFile/05章/src/exercise/practice5_2.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class practice5_2 {
+
+	public static void main(String[] args) {
+		double a = 30.51;
+		double b = 2.68;
+		System.out.println(Math.round(a / b));
+		System.out.println(Math.pow(a, b));
+		System.out.println(Math.min(a, b));
+		System.out.println(Math.sqrt(a) + Math.sqrt(b));
+		System.out.println(Math.random());
+	}
+
+}
