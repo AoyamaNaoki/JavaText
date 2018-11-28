f8ea6962b120b98c641bf1f46f59eba273aa033c
AoyamaNaoki <45162737+AoyamaNaoki@users.noreply.github.com>
2018-11-28 15:42:54 +0900

    7章

---
+++ ProjectFile/07章/src/exercise/practice7_2.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class practice7_2 {
+
+	public static void main(String[] args) {
+		double[] val = { 188.2, 175.6, 154.5, 168.2, 178.0 };
+		double mean;
+		for (int i = 0; i < val.length; i++) {
+			System.out.println(val[i]);
+			mean += val[i];
+		}
+
+	}
+
+}
